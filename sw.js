fe770e636c4fb928dd5c29c8da9be1e3e363e7e4
ehometech <ehometech2024@gmail.com>
2026-05-22 07:18:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
-const CACHE_NAME = 'kalk-v16';
-const ASSETS = ['./','./index.html','./app.js','./kfe-db.js','./manifest.json'];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
+// Service Worker - löscht alten Cache sofort
+const CACHE_NAME = 'kalk-v20';
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
+// Immer frisch vom Server - kein Caching
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request));
 });
